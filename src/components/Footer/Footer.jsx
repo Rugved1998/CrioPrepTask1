3e9cded4604a503ce9c55f86d21d1b16be76ab3a
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-08-07 08:18:23 +0530

    Header and Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+const Footer=()=>{
+return(
+    <div className={styles.footer}>
+    <small> CopyRights Reserved by @RugvedMudaliar</small>
+</div>
+);
+
+}
+
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+import styles from './Header.module.css';
+
+const Header=()=>{
+return(
+    <div className={styles.header}>
+    <h1> MultiStep Form</h1>
+</div>
+);
+
+}
+
+
+export default Header;
