e87ce1788afbb57913463b46bc9d4f550e4a862c
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-08-07 20:34:24 +0530

    Small Corrections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import styles from './Navbar.module.css';
 const Navbar=()=>{
 return(
     <nav className={styles.navbar}>
-    <NavButton text="< Left"/>
-    <NavButton text="Right >"/>
+    <NavButton text="<- Back"/>
+    <NavButton text="Next ->"/>
 </nav>
 );
 
--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -5,18 +5,12 @@ return(
     <div className={styles.content}>
         <h1> Personal Information Form</h1>
         <form className={styles.form}>
-            <div>
-                <label>Name:</label>
-                <input type ='text' placeholder='Enter your Name'/>
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type='text' placeholder='Enter your Email'/>
-            </div>
-            <div>
-                <label>Phone Number:</label>
-                <input type='text' placeholder='Enter your Phone Number'/>
-            </div>
+              
+            <input type ='text' placeholder='Enter your Name'/>
+           
+            <input type='text' placeholder='Enter your Email'/>
+                
+            <input type='text' placeholder='Enter your Phone Number'/>
     </form>
     </div>
 );
